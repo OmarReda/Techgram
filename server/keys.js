5b5c5f4e0c34f48af1e829c003f0c19b5a00e303
Omar Reda <34577933+OmarReda@users.noreply.github.com>
2020-06-05 01:13:24 +0200

    Server Side

---
+++ server/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://OmarReda:DG0eS2hGUNylVYSS@cluster0-fhtdq.mongodb.net/test?retryWrites=true&w=majority",
+  JWT_SECRET: "omarredasmaherbelal",
+};
---
+++ server/routes/auth.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../keys");
+const requireLogin = require("../middleware/requireLogin");
+
+router.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+router.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(422).json({ error: "Please add all the fields" });
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res
+          .status(422)
+          .json({ error: "User already exists with that email" });
+      }
+      bcrypt.hash(password, 12).then((hashedpassword) => {
+        const user = new User({
+          name,
+          email,
+          password: hashedpassword,
+        });
+        user
+          .save()
+          .then((user) => {
+            res.json({ message: "Saved Successfully" });
+          })
+          .catch((err) => {
+            consule.log(err);
+          });
+      });
+    })
+    .catch((err) => {
+      consule.log(err);
+    });
+});
+
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({ error: "Please add email or password" });
+  }
+  User.findOne({ email: email }).then((savedUser) => {
+    if (!savedUser) {
+      return res.status(422).json({ error: "User Not Found" });
+    }
+    bcrypt
+      .compare(password, savedUser.password)
+      .then((doMatch) => {
+        if (doMatch) {
+          //   res.json({ message: "Successfully Signed In" });
+          const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
+          const { id, name, email } = savedUser;
+          res.json({ token, user: { id, name, email } });
+        } else {
+          return res.status(422).json({ error: "Invalid Email Password" });
+        }
+      })
+      .catch((err) => {
+        consule.log(err);
+      });
+  });
+});
+
+module.exports = router;
